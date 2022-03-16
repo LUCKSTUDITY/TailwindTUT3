7ac7652a55de10c807b7903bc9366dc7c26fd16b
LUCKSTUDITY <sinhastudent10@gmail.com>
2022-03-16 10:17:00 +0530

    Third Commit

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      padding: {
+        '5px': '5px',
+        '6px': '3px',
+        '7px': '1px',
+      },
+      margin: {
+        '5px': '5px',
+        '6px': '3px',
+        '7px': '1px',
+      },
+      space: {
+        '5px': '5px',
+        '6px': '3px',
+        '7px': '1px',
+      }
+    },
+  },
+  plugins: [],
+}
